b82d291a894ca3386880252ee99e34cb132f1be6
ZahraJou <zahrajoulaei@gmail.com>
2024-05-24 15:42:20 -0400

    part 1 refactoring

--- part1-refactoring.js
+++ part1-refactoring.js
@@ -1,9 +1,29 @@
+// my old approach:
+
+// const csv =
+//   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+// const row = csv.split("\n");
+
+
+// for (i = 0; i < row.length; i++) {
+//   let cell = row[i].split(",");
+//   console.log(cell[0], cell[1], cell[2], cell[3]);
+// }
+
+// refactored code:
+
+
 const csv =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
-const row = csv.split("\n");
+const rows = csv.split("\n");
+const cell = rows.map(row => row.split(','));
 
 
-for (i = 0; i < row.length; i++) {
-  let cell = row[i].split(",");
-  console.log(cell[0], cell[1], cell[2], cell[3]);
-}
+// use foEach method instead of for loop:
+cell.forEach((z) => {
+    console.log(z)
+})
+
+
+
+
