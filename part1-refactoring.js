e57eff113a736360235632a56c063ce5beb5ca87
ZahraJou <zahrajoulaei@gmail.com>
2024-05-24 16:23:18 -0400

    part2 expanding functionality

--- part1-refactoring.js
+++ part1-refactoring.js
@@ -1,29 +1,22 @@
 // my old approach:
-
 // const csv =
 //   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 // const row = csv.split("\n");
-
-
 // for (i = 0; i < row.length; i++) {
 //   let cell = row[i].split(",");
 //   console.log(cell[0], cell[1], cell[2], cell[3]);
 // }
 
-// refactored code:
 
+// my refactored code:
 
 const csv =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 const rows = csv.split("\n");
-const cell = rows.map(row => row.split(','));
+const cells = rows.map((row) => row.split(","));
 
-
-// use foEach method instead of for loop:
-cell.forEach((z) => {
-    console.log(z)
-})
-
-
-
-
+// use forEach method instead of for loop:
+cells.forEach((cell) => {
+  const finalCell = cell.slice(0, 4);
+  console.log(finalCell);
+});
---
+++ part2-expandingFunctionality.js
@@ -0,0 +1,14 @@
+// Declare a variable that stores the number of columns in each row of data within the CSV.
+// Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+
+const csv =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+const rows = csv.split("\n");
+const cells = rows.map((row) => row.split(","));
+
+let numberOfCols = cells[0].length;
+
+cells.forEach((cell) => {
+  const finalCell = cell.slice(0, numberOfCols);
+  console.log(finalCell);
+});
